954c9f958622d933244fe8239cb45ae1ba504421
Khairul Alam <khairulalam@dell1721.5thbiz.local>
2022-07-04 16:21:48 -0400

    Added Detail.js, ExerciseVideos.js, SimilarExercises.js components and Youtube Search Rapid API URL

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetails = () => {
+
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+    }
+    fetchExercisesData();
+  }, [id])
+
   return (
-    <div>ExerciseDetails</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
