72ce4bc763a02a0fef7945d12531e2ef56dc0ab1
Khairul Alam <khairulalam@dell1721.5thbiz.local>
2022-07-06 16:13:34 -0400

    Finished exerciseDetails page deatils

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,16 +7,46 @@ import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({ exerciseDetail }) => {
     const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
+    const extraDetails = [
+        {
+            icon: BodyPartImage,
+            name: bodyPart
+        },
+        {
+            icon: TargetImage,
+            name: target
+        },
+        {
+            icon: EquipmentImage,
+            name: equipment
+        }
+    ]
+
     return (
         <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: "20px", alignItems: "center" }}>
             <img src={gifUrl} alt={name} loading="lazy" className='detail-image' />
             <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
-                <Typography fontSize="40px" fontWeight="900">
+                <Typography variant='h3'>
                     {name}
                 </Typography>
-                <Typography>
-                    Exercises keep you strong. {name} is one of the best exercises to target your abs. It will help you improve your mood and again energy.
+                <Typography variant='h6'>
+                    Exercises keep you strong. {name} is one of the best exercises to target your {target}. It will help you improve your mood and again energy.
                 </Typography>
+                {extraDetails.map((item) => {
+                    return (
+                        <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+                            <Button sx={{ background: '#fff2db', borderRadius: '50%', width: '100px', height: '100px' }}>
+                                <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px' }} />
+                            </Button>
+                            <Typography textTransform="capitalize" variant="h5">
+                                {item.name}
+                            </Typography>
+                        </Stack>
+                    )
+                })
+
+                }
             </Stack>
         </Stack>
     )
